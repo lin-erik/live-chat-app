4d4b3e00952e550bfac78b6f73ffe0b057b9b032
elin7780 <elin7780@bths.edu>
2018-07-08 19:40:20 -0400

    Handles user input for sending messages, created area for messages

--- components/Chat.js
+++ components/Chat.js
@@ -29,4 +29,34 @@ class Chat extends Component {
            });
     });
   }
+
+  handleKeyUp = (event) => {
+    const value = event.target.value;
+
+    if (event.keyCode === 13 && !event.shiftKey) {
+      const {activeUser: user} = this.props;
+      const chat = {user, message: value, timestamp: +new Date};
+
+      event.target.value = '';
+      axios.post('/message', chat);
+    }
+  }
+
+  render() {
+    return (
+      this.props.activeUser && <Fragment>
+
+      <div className='border-bottom border-gray w-100 d-flex align-items-center bg-white' style={ {height: 90} }>
+        <h2 className='text-dark mb-0 mx-4 px-2'>{this.props.activeUser}</h2>
+      </div>
+
+      <div className='border-top border-gray w-100 px-4 d-flex align-items-center bg-light' style={ {minHeight:90} }>
+        <textarea className='form-control px-3 py-2' onKeyUp={this.handleKeyUp} placeholder='Enter a message here' style={ {resize: 'none'}}></textarea>
+      </div>
+
+      </Fragment>
+    )
+  }
 }
+
+export default Chat;
