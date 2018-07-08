751cafef7dd21fef20aaaa32dba59ff0d65dcff1
elin7780 <elin7780@bths.edu>
2018-07-08 19:21:57 -0400

    Finished layout of user page and prompt, fixed default state - removed ; causing error

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 
 class IndexPage extends Component {
   state = {
-    user: null;
+    user: null
   }
 
   handleKeyUp = (event) => {
@@ -20,12 +20,38 @@ class IndexPage extends Component {
       background: 'transparent',
       color: '#999',
       border: 0,
-      border-bottom: '1px solid #666',
-      border-radius: 0,
-      font-size: '3rem',
-      font-weight: 500,
-      box-shadow: 'none !important'
+      borderBottom: '1px solid #666',
+      borderRadius: 0,
+      fontSize: '3rem',
+      fontWeight: 500,
+      boxShadow: 'none !important'
     };
 
+    return (
+      <Layout pageTitle='Chat'>
+        <main className='container-fluid position-absolute h-100 bg-dark'>
+          <div className='row position-absolute w-100 h-100'>
+            <section className='col-md-8 d-flex flex-row flex-wrap align-items-center align-content-center px-5'>
+              <div className='px-5 mx-5'>
+                <span className='d-block w-100 h1 text-light' style={ {marginTop: -50} }>
+                  { user ? ( <span>
+                               <span style={ {color: '#999'} }>Hello!</span> {user}
+                             </span> ) : 'What is your name?'
+                  }
+                </span>
+                
+                {!user && <input type='text' className='form-control mt-3 px-3 py-2' onKeyUp={this.handleKeyUp} autoComplete='off' style={nameInput} />}
+              </div>
+            </section>
+
+            <section className='col-md-4 position-relative d-flex flex-wrap h-100 align-items-start align-content-bwetween bg-white px-0'></section>
+          </div>
+        </main>
+      </Layout>
+    );
   }
 }
+
+export default () => (
+  <IndexPage />
+);
