374688202f2edb987056f6de6ddc2025ab7c169b
elin7780 <elin7780@bths.edu>
2018-07-08 21:58:30 -0400

    Working on rendering messages to page

---
+++ components/ChatMessage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ChatMessage extends Component {
+  render() {
+    const {position = 'left', message} = this.props;
+    const isRight = position.toLowerCase() === 'right';
+    const align = isRight ? 'text-right' : 'text-left';
+    const justify = isRight ? 'justify-content-end' : 'justify-content-start';
+
+    const messageBoxStyles = {
+      maxWidth: '70%',
+      flexGrow: 0
+    };
+  }
+}
